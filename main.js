440e40268dae1a152317e857ec986ac71b249fe3
thecreadil <thecreadil@gmail.com>
2022-04-25 12:06:11 +0000

    Fixed background images

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
 // fetch('./data.json').then(result => result.json()).then(console.log)
-let currentAmount = document.querySelectorAll(".currentAmount")
-let previousAmount = document.querySelectorAll(".previousAmount")
-let period = document.querySelectorAll(".period")
-
-
-
+let currentAmount = document.querySelectorAll(".currentAmount");
+let previousAmount = document.querySelectorAll(".previousAmount");
+let period = document.querySelectorAll(".period");
 
 async function getData(period) {
-    const response = await fetch("./data.json")
-    const data = await response.json()
-    // console.log(data)
-    switch (period) {
-        case "weekly":
-            {
-                currentAmount.forEach(activity=>{
-                    // Assign the current value to each activity by testing for currentAmount class name
-                    // switch (activity.has)
-                })
-            }
-            
-            break;
-    
-        default:
-            break;
-    }
+  const response = await fetch("./data.json");
+  const data = await response.json();
+  //   console.log(data);
+  //   return data;
+  for (let key in currentAmount) {
+    currentAmount[key].innerText = data[key]["timeframes"][period]["current"];
+  }
+
+  for (let key in previousAmount) {
+    previousAmount[key].innerText = data[key]["timeframes"][period]["previous"];
+  }
 }
 
-getData()
+getData("weekly");
